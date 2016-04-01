b94435e027141a917fe0b8eecc9b5ef3454bf002
RainyRuRu <believeu123@gmail.com>
2016-04-01 14:24:25 +0800

    add buttonPanel and buttons

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -20,6 +20,10 @@ public class SudokuSolver extends JFrame {
 	private BoardRenderer mainBoard  = new BoardRenderer(BoardRenderer.BoardType.MAIN_BOARD);
 	private ResultRenderer gaResultRenderer  = new ResultRenderer(450, 200, 500, 50, "GA");
 	private ResultRenderer forceResultRenderer  = new ResultRenderer(450, 200, 500, 300, "Force");
+	private JButton example1Btn = new JButton("範例1");
+	private JButton example2Btn = new JButton("範例2");
+	private JButton checkBtn = new JButton("清除");
+	private JButton resetBtn = new JButton("解題");
 	
 	public SudokuSolver (String title) {
 		super(title);
@@ -32,18 +36,30 @@ public class SudokuSolver extends JFrame {
 		this.setPreferredSize(new Dimension(size_width, size_height));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// 關閉視窗時離開主程式
 		this.setLocationRelativeTo(null);						// 開啟時顯示於螢幕正中央
-		this.setAlwaysOnTop(true);							// 顯示於最上方
+		this.setAlwaysOnTop(true);                              // 顯示於最上方
 		this.setResizable(false);								// 不可被調整大小
+			
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setSize(400,50);
+		buttonPanel.setLocation(50, 470);
+		buttonPanel.setBackground(null);
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(example1Btn);
+		buttonPanel.add(example2Btn);
+		buttonPanel.add(checkBtn);
+		buttonPanel.add(resetBtn);
+		
 		
 		Container mainPanel = this.getContentPane();
-		mainPanel.setBackground(Color.LIGHT_GRAY);
+		mainPanel.setBackground(null);
 		mainPanel.setLayout(null);
 		
 		// 新增面板
 		mainPanel.add(mainBoard);            // 棋盤 400*400
 		mainPanel.add(gaResultRenderer);     // 基因演算法結果視窗 450*200
 		mainPanel.add(forceResultRenderer);	 // 暴力解結果視窗 450*200
-		
+		mainPanel.add(buttonPanel);
+
 		// 顯示視窗
 		this.pack();
 		this.setVisible(true);
